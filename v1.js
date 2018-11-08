a3b54f81423020d2b3909a5a0bc87be0318d81d6
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-08 16:39:10 -0500

    Update v1.js

--- v1.js
+++ v1.js
@@ -1,38 +1,59 @@
-function vld(elm,n){if(elm != null){return elm[n];}else{return '';}}
-var nameArr = ["Aaron","Abigail","Adam","Adrian","Adrienne","Aimee","Alan","Albert","Alejandro","Alex","Alexander","Alexandra","Alexis","Alice","Alicia","Alisha","Alison","Allen","Allison","Alyssa","Amanda","Amber","Amy","Ana","Andre","Andrea","Andrew","Angel","Angela","Angelica","Angie","Anita","Ann","Anna","Anne","Annette","Anthony","Antonio","April","Arthur","Ashlee","Ashley","Audrey","Austin","Autumn","Barbara","Barry","Becky","Benjamin","Beth","Bethany","Betty","Beverly","Billy","Blake","Bobby","Bonnie","Brad","Bradley","Brandi","Brandon","Brandy","Brenda","Brendan","Brent","Brett","Brian","Brianna","Bridget","Brittany","Brittney","Brooke","Bruce","Bryan","Caitlin","Caleb","Calvin","Cameron","Candace","Candice","Carl","Carla","Carlos","Carmen","Carol","Caroline","Carolyn","Carrie","Casey","Cassandra","Cassie","Catherine","Cathy","Chad","Charles","Chase","Chelsea","Cheryl","Chris","Christian","Christie","Christina","Christine","Christopher","Christy","Cindy","Claudia","Clayton","Clifford","Clinton","Cody","Colin","Colleen","Connie","Corey","Cory","Courtney","Craig","Cristina","Crystal","Curtis","Cynthia","Dale","Damon","Dana","Daniel","Danielle","Danny","Darrell","Darren","David","Dawn","Dean","Deanna","Deborah","Debra","Denise","Dennis","Derek","Derrick","Desiree","Devin","Diana","Diane","Dominique","Donald","Donna","Doris","Dorothy","Douglas","Drew","Duane","Dustin","Dwayne","Dylan","Ebony","Eddie","Edward","Edwin","Elizabeth","Emily","Emma","Eric","Erica","Erik","Erika","Erin","Ernest","Ethan","Eugene","Evan","Evelyn","Felicia","Fernando","Frances","Francisco","Frank","Frederick","Gabriel","Garrett","Gary","Geoffrey","George","Gerald","Gina","Glenn","Gloria","Grace","Grant","Gregory","Hannah","Harold","Harry","Heather","Hector","Heidi","Helen","Henry","Holly","Howard","Ian","Isaac","Jack","Jaclyn","Jacob","Jacqueline","Jaime","James","Jamie","Jane","Janet","Janice","Jared","Jasmine","Jason","Javier","Jay","Jean","Jeanette","Jeff","Jeffery","Jeffrey","Jenna","Jennifer","Jenny","Jeremiah","Jeremy","Jermaine","Jerome","Jerry","Jesse","Jessica","Jesus","Jill","Jillian","Jimmy","Joan","Joanna","Jodi","Jody","Joe","Joel","John","Johnathan","Johnny","Jon","Jonathan","Jonathon","Jordan","Jorge","Jose","Joseph","Joshua","Joy","Joyce","Juan","Judith","Judy","Julia","Julian","Julie","Justin","Kara","Karen","Kari","Katelyn","Kath","Katherine","Kathleen","Kathryn","Kathy","Katie","Katrina","Kayla","Keith","Kelli","Kellie","Kelly","Kelsey","Kendra","Kenneth","Kerri","Kerry","Kevin","Kim","Kimberly","Krista","Kristen","Kristi","Kristie","Kristin","Kristina","Kristine","Kristopher","Kristy","Krystal","Kurt","Kyle","Lacey","Lance","Larry","Latasha","Latoya","Laura","Lauren","Laurie","Lawrence","Leah","Lee","Leonard","Leslie","Linda","Lindsay","Lindsey","Lisa","Logan","Lori","Louis","Lucas","Luis","Luke","Lynn","Madison","Mallory","Mandy","Manuel","Marc","Marcus","Margaret","Maria","Marie","Marilyn","Mario","Marissa","Mark","Martha","Martin","Marvin","Mary","Mathew","Matthew","Maurice","Meagan","Megan","Meghan","Melanie","Melinda","Melissa","Melvin","Meredith","Michael","Micheal","Michele","Michelle","Miguel","Mindy","Miranda","Misty","Mitchell","Molly","Monica","Monique","Morgan","Nancy","Natalie","Natasha","Nathan","Nathaniel","Neil","Nicholas","Nichole","Nick","Nicole","Nina","Noah","Olivia","Omar","Oscar","Pamela","Patricia","Patrick","Paul","Paula","Peter","Philip","Phillip","Priscilla","Rachael","Rachel","Ralph","Randall","Randy","Raymond","Rebecca","Rebekah","Regina","Reginald","Renee","Rhonda","Ricardo","Richard","Ricky","Robert","Roberto","Robin","Robyn","Rodney","Roger","Ronald","Ronnie","Rose","Ross","Roy","Ruben","Russell","Ruth","Ryan","Sabrina","Samantha","Samuel","Sandra","Sara","Sarah","Scott","Sean","Sergio","Seth","Shane","Shannon","Sharon","Shaun","Shawn","Shawna","Sheena","Sheila","Shelley","Shelly","Sheri","Sherri","Sherry","Shirley","Sonia","Sonya","Spencer","Stacey","Stacie","Stacy","Stanley","Stefanie","Stephanie","Stephen","Steve","Steven","Susan","Suzanne","Tabitha","Tamara","Tammy","Tanya","Tara","Tasha","Taylor","Teresa","Terrance","Terrence","Terri","Terry","Theodore","Theresa","Thomas","Tiffany","Timothy","Tina","Todd","Tommy","Toni","Tony","Tonya","Tracey","Traci","Tracie","Tracy","Travis","Trevor","Tricia","Troy","Tyler","Tyrone","Valerie","Vanessa","Veronica","Victor","Victoria","Vincent","Virginia","Walter","Wanda","Wayne","Wendy","Wesley","Whitney","William","Willie","Yolanda","Yvonne","Zac"];
+function vld(elm, n) {    if (elm != null) {      return elm[n];    } else {      return '';    }  }
+
+function jpReady(str) {    if (str != undefined) {      return str.replace(/\?/g, '_QST_').replace(/\&/g, '_AMP_').replace(/\#/g, '_HSH_');    } else {      return '';    }  }
+
+function cleanMatches(str) {    if (str != undefined) {      let matches = str.match(/(?<=<a href="http.{0,1}:\/\/).+?(?=")/g);      if (matches != null) {        let arr = Array.from(matches).map(el => {          return jpReady(el.replace(/\/$/, '').replace(/.+wikipedia.+|.+www.w3.org.+|.+microsoft.com.+/g, ''));        });        return arr.filter(String);      } else {        return '';      }    } else {      return '';    }  }
 
 
-var outArrObj = [];
+var nameArr = ["Brittney", "Cristina", "Elizabeth"];
 
-var items = JSON.parse(document.body.innerText).items;
-var s_geo = "dallas";
-var regXgeo = new RegExp(s_geo, "i");
-for(i=0; i<items.length; i++){
-	let geo = jpReady(items[i].location);
-	let about = items[i].about_me;
-	let twitter = jpReady(vld(/(?<=twitter\.com\/)\w+|(?<=twitter.{0,10}?\(at\))\w+|(?<=twitter.{0,10}?@)\w+/.exec(about),0));
-	let email = jpReady(vld(/(?<=>|\s)\S+@\w+\.\S+(?=<)/.exec(about),0));
-	let weblinks = cleanMatches(about);
+function opener(elm, n) {
+  setTimeout(() => {
+    var searchLink = "https://api.stackexchange.com/2.2/users?pagesize=99&order=desc&sort=reputation&inname=" + elm + "&site=stackoverflow&filter=XJ.7f)7Zg)UC4&page=3";
+
+    var wnd = window.open(searchLink);
 	
-	if(regXgeo.test(geo) === true){
-		outArrObj.push({"fn": items[i].display_name, "geo": geo, "id": items[i].user_id, "cd": items[i].creation_date, "git": items[i].link, "tw": twitter, "em": email, "web": weblinks});
-	}
+    setTimeout(() => {
+      var outArrObj = [];
+      var items = JSON.parse(wnd.document.body.innerText).items;
+      var s_geo = "Boston";
+      var regXgeo = new RegExp(s_geo, "i");
+      for (i = 0; i < items.length; i++) {
+        let geo = jpReady(items[i].location);
+        let about = items[i].about_me;
+        let twitter = jpReady(vld(/(?<=twitter\.com\/)\w+|(?<=twitter.{0,10}?\(at\))\w+|(?<=twitter.{0,10}?@)\w+/.exec(about), 0));
+        let email = jpReady(vld(/(?<=>|\s)\S+@\w+\.\S+(?=<)/.exec(about), 0));
+        let weblinks = cleanMatches(about);
+        let stackLink = vld(/(?<=stackoverflow\.com\/users\/).+/.exec(items[i].link), 0);
+        if (regXgeo.test(geo) === true) {
+          outArrObj.push({
+            "fn": items[i].display_name,
+            "geo": geo,
+            "id": items[i].user_id,
+            "cd": items[i].creation_date,
+            "st": stackLink,
+            "tw": twitter,
+            "em": email,
+            "web": weblinks
+          });
+        }
+      }
+
+      var out = JSON.stringify(outArrObj);
+      console.log(out);
+	if(outArrObj.length > 0){
+      var winjp = window.open("https://script.google.com/macros/s/AKfycbx67X5G-_PW9URN8JkxKAIZYYHSx0_23ukD2CQ9-Pt0Z5K33sjA/exec?o=" + out);
+    }
+      setTimeout(() => {
+        winjp.close();
+      }, 2000);
+    }, 900);
+    setTimeout(() => {
+      wnd.close();
+    }, ((n+1) * 2100));
+
+  }, ((n+1) * 1000));
 }
-
-console.log(outArrObj);
-
-function jpReady(str){
-if(str != undefined){
-	return str.replace(/\?/g, '_QST_').replace(/\&/g, '_AMP_').replace(/\#/g, '_HSH_');
-}else{return '';}
-}
-
-function cleanMatches(str){
-	if(str != undefined){
-		let matches = str.match(/(?<=<a href="http.{0,1}:\/\/).+?(?=")/g);
-		if(matches != null){
-			let arr = Array.from(matches).map(el=>{ return jpReady(el.replace(/\/$/, '').replace(/.+wikipedia.+|.+www.w3.org.+|.+microsoft.com.+/g, ''));});
-			return arr.filter(String);
-		}else{return '';}
-	}else{return '';}
+for (a = 0; a < nameArr.length; a++) {
+  opener(nameArr[a], a);
 }
